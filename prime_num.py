58667aace469ab4cabc87c103cb6a09106867346
davebenedict <dave.d.benedict@gmail.com>
2024-04-23 12:02:28 -0600

    updated unit tests for negative, one, and float

--- prime_num.py
+++ prime_num.py
@@ -1,8 +1,12 @@
 import math
 
 def is_prime(num):
+    
+    if not isinstance(num, int) or (num)<2:
+        return False
     for i in range(2, int(math.sqrt(num))+1):
         if num%i==0:
             return False
     return True
+
     
--- test_prime.py
+++ test_prime.py
@@ -3,13 +3,23 @@ import unittest
 from prime_num import is_prime
 
 class TestPrime(unittest.TestCase):
+    def test_float(self):
+        self.assertFalse(is_prime(11.1))
+    def test_zero(self):
+        self.assertFalse(is_prime(0))
+    def test_negative(self):
+        self.assertFalse(is_prime(-1))
+    def test_one(self):
+        self.assertFalse(is_prime(1))
     def test_two(self):
         self.assertTrue(is_prime(2))
+    def test_four(self):
+        self.assertFalse(is_prime(4))
     def test_five(self):
         self.assertTrue(is_prime(5))
     def test_nine(self):
         self.assertFalse(is_prime(9))
-    def tests_eleven(self):
+    def test_eleven(self):
         self.assertTrue(is_prime(11))
 
 if __name__=='__main__':
